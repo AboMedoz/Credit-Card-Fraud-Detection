f3da7c89204ce7a4642710654af6be8dd84c62c2
mahmoud radwan <medodiscordban@gmail.com>
2025-03-11 22:40:42 +0200

    IT WORKED

---
+++ test.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from logistic_regression import LogisticRegression
+
+
+x, y = make_classification(n_samples=1000, n_features=5, n_classes=2, random_state=42)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_predict) * 100
+print(f'Accuracy: {accuracy:.2f}')
